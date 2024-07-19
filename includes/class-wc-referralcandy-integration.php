2eeb1d216d86b0b927f84b16c13b4aeac2866258
Shanhan <thecsh3@gmail.com>
2024-07-19 18:15:20 +0800

    fix: remove custom metadata fields on subsequent orders
    fix: do not access id directly for non hpos

--- includes/class-wc-referralcandy-integration.php
+++ includes/class-wc-referralcandy-integration.php
@@ -176,11 +176,30 @@ if (!class_exists('WC_Referralcandy_Integration')) {
             }
         }
 
+        private function remove_custom_metadata($order, $type)
+        {
+            // If order is carrying over previous custom metadata fields, remove them
+            $order_meta_data = $order->get_meta_data();
+            $meta_keys_to_remove = ['rc_accepts_marketing', 'rc_loc', 'rc_aic'];
+
+            foreach ($order_meta_data as $meta_data) {
+                if (in_array($meta_data->key, $meta_keys_to_remove)) {
+                    if ($type == 'post') {
+                        delete_post_meta($order->get_id(), $meta_data->key);
+                    } else if ($type == 'order') {
+                        $order->delete_meta_data($meta_data->key);
+                    }
+                }
+            }
+        }
+
         public function update_order_meta($order)
         {
             $checkout_fields = Package::container()->get( CheckoutFields::class );
             $rc_accepts_marketing_field = $checkout_fields->get_field_from_object($this->accepts_marketing_field_id, $order);
+
             if (OrderUtil::custom_orders_table_usage_is_enabled()) {
+                $this->remove_custom_metadata($order, 'order');
                 if (!empty($rc_accepts_marketing_field)) {
                     $order->update_meta_data('rc_accepts_marketing', $rc_accepts_marketing_field);
                 }
@@ -194,10 +213,10 @@ if (!class_exists('WC_Referralcandy_Integration')) {
                     }
                 }
                 $order->save();
-
             } else {
+                $this->remove_custom_metadata($order, 'post');
                 if (!empty($rc_accepts_marketing_field)) {
-                    update_post_meta($order->id, 'rc_accepts_marketing', $rc_accepts_marketing_field);
+                    update_post_meta($order->get_id(), 'rc_accepts_marketing', $rc_accepts_marketing_field);
                 }
             }
         }
