b9dedb688129273585d436e2bebd2401709d41fe
chingyeowp <chingyeow10@gmail.com>
2025-01-31 11:35:08 +0800

    update version

--- woocommerce-referralcandy.php
+++ woocommerce-referralcandy.php
@@ -6,7 +6,7 @@
  * Author: ReferralCandy
  * Author URI: http://www.referralcandy.com
  * Text Domain: woocommerce-referralcandy
- * Version: 2.5.2
+ * Version: 2.5.3
  * Requires at least: 6.4
  * Requires PHP: 7.4
  * Tested up to: 6.6
